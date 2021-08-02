4eb9f60797c107f402c3ae1cc0cb467f43ecf5b0
Tim Godin <support@kitebot.ca>
2021-08-02 16:42:17 -0600

    added current fiddlings

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,20 @@
-<div>Welcome. Looking to take on short term, remote work contracts. Preference to svelte/typescript projects.</div>
+<div>
+    <p>
+        Welcome. 
+        Looking to take on short term, remote work contracts. 
+        Preference to svelte/typescript projects.
+    </p>
+    <br>
+    <br>
+    <p>
+        Stuff I've been fiddling with lately:
+    </p>
+    <ul>
+        <li><a href="https://metta.d3nvj95fro2i68.amplifyapp.com/">playfab + svelte example repo</a></li>
+        <li><a href="https://metta.d3ofg83mivrlli.amplifyapp.com/">word-colouring api</a></li>
+        <li><a href="https://metta.d2z1xs5jqfi6cq.amplifyapp.com/">word game prototype</a></li>
+        <li><a href="https://metta.d2z1xs5jqfi6cq.amplifyapp.com/widgets/runewords2">runeword calculator</a></li>
+    </ul>
+    
+
+</div>
