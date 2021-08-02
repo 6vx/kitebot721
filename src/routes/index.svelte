ef2359967de6b617708d8064fd04c546e08e6003
Tim Godin <support@kitebot.ca>
2021-08-02 16:47:32 -0600

    try fire

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,7 @@
     </p>
     <ul>
         <li><a href="https://metta.d3nvj95fro2i68.amplifyapp.com/">playfab + svelte example repo</a></li>
-        <li><a href="https://metta.d3ofg83mivrlli.amplifyapp.com/">word-colouring api</a></li>
+        <li><a href="https://metta.d3ofg83mivrlli.amplifyapp.com/">word-colouring api</a> (try 'fire' :)</li>
         <li><a href="https://metta.d2z1xs5jqfi6cq.amplifyapp.com/">word game prototype</a></li>
         <li><a href="https://metta.d2z1xs5jqfi6cq.amplifyapp.com/widgets/runewords2">runeword calculator</a></li>
     </ul>
