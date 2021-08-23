826a0c5a5d647c87031852018dfeee7ea40aba85
Tim Godin <support@kitebot.ca>
2021-08-22 20:54:51 -0600

    meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,8 @@
 
 <svelte:head>
     <title>kiteBot</title>
+    <meta name="description" content="Canadian developer, word game nerd who loves running and jumping and making widgets on my computer the rest of the time.">
+    <meta name="keywords" content="calgary, kelowna, canada, canadian, software, developer, kitebot, playfab + svelte, deathmetta, xbox, microsoft, word game, svelte, playfab">
 </svelte:head>
 
 
