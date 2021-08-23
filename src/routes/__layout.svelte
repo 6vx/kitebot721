d146b8b39ccd5b64f131834e23f37a114e0be116
Tim Godin <support@kitebot.ca>
2021-08-22 22:21:46 -0600

    change heading

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,7 +11,7 @@
 </svelte:head>
 
 
-<p>kite<a href="/">Bot</a>/tim<a href="/info/about">Godin</a></p>
+<h1>kite<a href="/info/about">Bot</a></h1>
 <p><span><a href="/info">info</a> - <a href="/devlog">devlog</a> - <a href="/info/contact">contact</a> - <a href="/books">reading</a></span></p>
 <slot></slot>
 
